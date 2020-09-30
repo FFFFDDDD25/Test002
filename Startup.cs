ee60b2942c1d916d3456354fb90f0d8a9a606383
FFFFDDDD25 <phone993825@gmail.com>
2020-09-30 13:56:40 +0800

    foo

--- Startup.cs
+++ Startup.cs
@@ -39,7 +39,7 @@ namespace Test002
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World! ASP.NET~~     9        \n");
+                    await context.Response.WriteAsync("Hello World! ASP.NET~~     10        \n");
                 });
 
                 endpoints.MapGet("/dave", async context =>
--- makefile
+++ makefile
@@ -18,7 +18,7 @@ preview3:
 	docker build -t aspnetapp .
 	docker run -d -p 8080:80 --name Test002 aspnetapp
 
-
+	
 
 
 
