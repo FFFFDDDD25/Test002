18f2df5d8b03179296e00d2969297b6c0cee845d
FFFFDDDD25 <phone993825@gmail.com>
2020-11-21 23:40:59 +0800

    foo

--- 2.bat
+++ 2.bat
@@ -2,5 +2,5 @@
     powershell -command "docker rm $(docker stop $(docker ps -a -q --filter ancestor=test002 --format=\"{{.ID}}\"))"
     git pull
     docker build  . -t test002
-    docker run --name test5566 -p 5566:8080 test002
+    docker run --name test5566 -p 5566:8081 test002
 pause
